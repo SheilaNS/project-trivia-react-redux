567fdd60b25b197094443db660d54921847ccb16
Gustavo Rondello <gustavorondello99@outlook.com>
2022-04-06 14:41:24 -0300

    [Grupo 9] Main Branch

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 
+// Commit Inicial
+
 const Routes = () => (
   <Switch>
     <Route component={ Login } path="/" exact />
